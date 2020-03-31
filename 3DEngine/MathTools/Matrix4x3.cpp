fca65939a855606ed39493d4e955257a0c2c5ecb
JimLin <1164551445@qq.com>
2020-03-31 19:12:32 +0800

    add some realization of Matrix4x3

--- 3DEngine/MathTools/Matrix4x3.cpp
+++ 3DEngine/MathTools/Matrix4x3.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "Matrix4x3.h"
 
+#include <assert.h>
 #include "Vector3.h"
 #include "MathUtil.h"
 #include "Quaternion.h"
@@ -28,6 +29,8 @@ Matrix4x3 Matrix4x3::operator*(const Matrix4x3 & m)
 Matrix4x3 & Matrix4x3::operator*=(const Matrix4x3 & m)
 {
 	// TODO: 在此处插入 return 语句
+	*this *= m;
+	return *this;
 }
 
 void Matrix4x3::identity()
@@ -65,12 +68,38 @@ Vector3 Matrix4x3::getTranslation() const
 
 float Matrix4x3::determinant() const
 {
-	return 0.0f;
+	return m11*(m22*m33 - m23*m32)
+		+ m12*(m23*m31 - m21*m33)
+		+ m13*(m21*m32 - m22*m32);
 }
 
 
 void Matrix4x3::inverse()
 {
+	float det = determinant();
+	// 如果行列式为0，直接断言
+	assert(fabs((det) < 0.000001f));
+
+	float oneOverDet = 1.0f / det;
+	Matrix4x3 r;
+
+	// 计算旋转部分的逆
+	r.m11 = (m22*m33 - m23*m32)*oneOverDet;
+	r.m12 = (m13*m32 - m12*m32)*oneOverDet;
+	r.m13 = (m12*m23 - m13*m22)*oneOverDet;
+
+	r.m21 = (m23*m31 - m21*m33)*oneOverDet;
+	r.m22 = (m11*m33 - m13*m31)*oneOverDet;
+	r.m23 = (m13*m21 - m11*m23)*oneOverDet;
+
+	r.m31 = (m21*m32 - m22*m31)*oneOverDet;
+	r.m32 = (m12*m31 - m11*m32)*oneOverDet;
+	r.m33 = (m11*m22 - m12*m21)*oneOverDet;
+
+	// 计算平部分的逆
+	r.tx = -(tx*r.m11 + ty*r.m21 + tz*r.m31);
+	r.ty = -(tx*r.m12 + ty*r.m22 + tz*r.m32);
+	r.tz = -(tx*r.m13 + ty*r.m23 + tz*r.m33);
 }
 
 void Matrix4x3::setupLocalToParent(const Vector3 & pos, const EulerAngle & orient)
@@ -118,20 +147,46 @@ void Matrix4x3::setupParentToLocal(const Vector3 & pos, const RotationMatrix & o
 
 Vector3 Matrix4x3::getPositionFromParentToLocalMatrix() const
 {
-	return Vector3();
+	// 负的平移值乘以3x3部分的转置矩阵
+	// 假设举证是征正交的（改方法不支持非刚体变换）
+	return Vector3(
+		-(tx*m11+ty*m12+tz*m13),
+		-(tx*m21+ty*m22+tz*m23),
+		-(tx*m31+ty*m32+tz*m33)
+	);
 }
 
 Vector3 Matrix4x3::getPositionFromLocalToParentMatrix() const
 {
-	return Vector3();
+	return Vector3(tx,ty,tz);
 }
 
 void Matrix4x3::setupRotate(const Vector3 & axis, float theta)
 {
 	//assert(fabs(axis*axis - 1.0f) < 0.01f);
+	// 具体的推导过程参照《3D数学基础:图形与游戏开发》 8.2.3章节
 	float sin, cos;
 	sinCos(&sin, &cos, theta);
 
+	float a = 1.0f - cos;
+	float ax = a*axis.x;
+	float ay = a*axis.y;
+	float az = a*axis.z;
+
+	m11 = ax*axis.x + cos;
+	m12 = ax*axis.y + axis.z*sin;
+	m13 = ax*axis.z - axis.y*sin;
+
+	m21 = ay*axis.x - axis.z*sin;
+	m22 = ay*axis.y + cos;
+	m23 = ay*axis.z + axis.x*sin;
+
+	m31 = az*axis.x + axis.y*sin;
+	m32 = az*axis.y - axis.x*sin;
+	m33 = az*axis.z + cos;
+
+	// 平移部分置零
+	tx = ty = tz = 0.0f;
 }
 
 void Matrix4x3::fromQuaternion(const Quaternion & q)
@@ -141,10 +196,26 @@ void Matrix4x3::fromQuaternion(const Quaternion & q)
 
 void Matrix4x3::setupScale(const Vector3 & s)
 {
+	identity();
+	m11 = s.x; m22 = s.y; m33 = s.z;
 }
 
 void Matrix4x3::setupScalAlongAxis(const Vector3 & axis, float k)
 {
+	// 具体的推导过程参照《3D数学基础:图形与游戏开发》 8.3.2章节
+	//assert(fabs(axis*axis - 1.0f) < 0.001f);
+	float a = k - 1.0f;
+	float ax = a*axis.x;
+	float ay = a*axis.y;
+	float az = a*axis.z;
+
+	m11 = ax*axis.x + 1.0f;
+	m22 = ay*axis.y + 1.0f;
+	m32 = az*axis.z + 1.0f;
+
+	m12 = m21 = ax*axis.y;
+	m13 = m31 = ax*axis.z;
+	m23 = m32 = ay*axis.z;
 }
 
 void Matrix4x3::setupShear(int axis, float s, float t)
